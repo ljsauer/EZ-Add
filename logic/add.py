b81b1f206c1ffe91b6e26749a4d6b693c9467c90
Lillie Sauer <lilliejsauer@gmail.com>
2021-09-06 15:28:49 -0500

    tidy up

--- logic/add.py
+++ logic/add.py
@@ -1,14 +1,13 @@
 from typing import List
-
-from .clean import RegexCleaner
+from clean import RegexCleaner
 
 
 class DollarAdder(object):
     def __init__(self):
-        self.NLPCleaner = RegexCleaner()
+        self.cleaner = RegexCleaner()
 
     def get_dollar_amts(self, raw_input: str) -> float:
-        cleaned_output: List[float] = self.NLPCleaner.clean(raw_input)
+        cleaned_output: List[float] = self.cleaner.clean(raw_input)
         total = 0.0
         for amt in cleaned_output:
             total += amt
