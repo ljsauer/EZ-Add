ad569acf5e5018761ea4e3d1df3b894cf9ed45ce
Lillie Sauer <lilliejsauer@gmail.com>
2021-09-06 15:16:16 -0500

    Update clean.py

--- logic/clean.py
+++ logic/clean.py
@@ -4,9 +4,10 @@ import re
 
 
 class RegexCleaner:
-    filter = re.compile(r'\$\d+(?: \.\d+)?')
+    filter = re.compile(r'\$\d+(?:\.\d+)?')
 
     def clean(self, raw_input: str) -> List[float]:
+        raw_input = raw_input.replace(" ", "")
         dollars_string: List = [amt.strip("$") for amt in self.filter.findall(raw_input)]
         dollars_floats = [float(amt) for amt in dollars_string]
 
